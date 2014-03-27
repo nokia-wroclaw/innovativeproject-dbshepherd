4c9870e154d84f089fd52542cee44d7dad443155
bleakdev <sebastian.zientek1@gmail.com>
2014-03-27 19:56:11 +0100

    Parsowanie argumentów

--- dbmodules/m_core.py
+++ dbmodules/m_core.py
@@ -1,10 +1,27 @@
 import cmd
+import re
+
+
+class ParseArgsException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
 
 class ModuleCore(cmd.Cmd):
     def set_name(self, name):
         self.prompt = "[" + name + "]>"
 
-    def do_exit(self,*args):
+    def parse_args(self, string="", n=0):
+        list = re.findall('"+.*"+|[a-zA-Z0-9!@#$%^&*()_+-,./<>?]+', string)
+        if len(list) == n or n == 0:
+            r_list = []
+            for l in list:
+                r_list.append(l.replace('"', ''))
+            return r_list
+        else:
+            raise ParseArgsException("Nieodpowiednia ilość argumentów")
+
+
+    def do_exit(self, *args):
         return True
 
     def do_EOF(self, line):
--- dbmodules/postgres.py
+++ dbmodules/postgres.py
@@ -1,6 +1,8 @@
 from dbmodules import m_core
 
+
 import sys
+
 sys.path.append("..")
 
 import alias
@@ -12,22 +14,31 @@ class Postgres(m_core.ModuleCore):
         self.set_name('Postgres')
 
     def do_query(self, args):
-        values = args.split(' ')
-        ali = alias.Alias(values[0])
-        adr = ali.show(values[1].split('.')[0])["connection"]["adress"]
-        usr = ali.show(values[1].split('.')[0])[values[1].split('.')[1]]["user"]
-        pwd = ali.show(values[1].split('.')[0])[values[1].split('.')[1]]["passwd"]
-        db_name = ali.show(values[1].split('.')[0])[values[1].split('.')[1]]["name"]
-
         try:
-            conn = psycopg2.connect(dbname=db_name,user=usr,host=adr,password=pwd, port=1234)
-            cur = conn.cursor()
-            cur.execute(values[2])
-            rows = cur.fetchall()
+            values = self.parse_args(args, 3)
+            try:
+                ali = alias.Alias(values[0])
+            except:
+                print("Nie można otworzyć pliku konfiguracyjnego:", values[0])
+        except m_core.ParseArgsException as e:
+            print(e)
 
-            for row in rows:
-                for cell in row:
-                    print(cell, "   ", end='')
-                print("\n");
-        except:
-            print("Problem z połączeniem!")
+
+
+        # adr = ali.show(values[1].split('.')[0])["connection"]["adress"]
+        # usr = ali.show(values[1].split('.')[0])[values[1].split('.')[1]]["user"]
+        # pwd = ali.show(values[1].split('.')[0])[values[1].split('.')[1]]["passwd"]
+        # db_name = ali.show(values[1].split('.')[0])[values[1].split('.')[1]]["name"]
+        #
+        # try:
+        #     conn = psycopg2.connect(dbname=db_name,user=usr,host=adr,password=pwd, port=1234)
+        #     cur = conn.cursor()
+        #     cur.execute(values[2])
+        #     rows = cur.fetchall()
+        #
+        #     for row in rows:
+        #         for cell in row:
+        #             print(cell, "   ", end='')
+        #         print("\n");
+        # except:
+        #     print("Problem z połączeniem!")
