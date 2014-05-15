2862029d4bd4046d9fa3c84744c4f4631667eb00
kaxiorek <kaxiorek@gmail.com>
2014-05-15 23:08:29 +0200

    Usprawnienie laczenia sie do listy, servera i wszystkiego

--- db-shepherd.py
+++ db-shepherd.py
@@ -45,32 +45,43 @@ class Shell(cmd.Cmd):
         else:
             completions = [f for f in self.modules if f.startswith(text)]
         return completions
-
-#adres_user_password_sshport_remoteport
+   
     def do_connect(self, arg):
-        """Connecting via ssh"""
-        conf_file, server_name = arg.split()
-        try:
-            conf = ConfigManager(conf_file)
-            connection =  conf.show(server_name)["connection"]
-            command = connection["adress"] + "_" + connection["user"]+ "_" + \
-                    connection["passwd"] + "_" + str(connection["sshport"])  + "_" + str(connection["remoteport"]) 
-            conn.send(command)
-            t = None
-            while t == None:
-                t = conn.get_state()
-            print(t)
-        except Exception as e:
-            print (e)
-        
-    def do_connectToList(self,arg):
-        list_of_lists = arg.split(" ")
-        for lista in list_of_lists:
-            path = lista + ".yaml"
-            try:
-                self.connectToList(path)
-            except ConfigManagerError as e:
-                print (e)
+        "Connect to (all), list, list.server"
+        if arg == "":
+            print ("connect to all...")
+            import os
+            for file in os.listdir("config"):
+                if file.endswith(".yaml"):
+                    list_name = file.title()[:file.rfind(".")]
+                    print("connecting to...", list_name)
+        else:   
+            params = arg.split(".")
+            if len(params) == 1:
+                print ("connect list...")
+                self.connectList("config/"+params[0]+".yaml")
+            elif len(params) == 2:
+                print ("connect serv...")
+                try:
+                    conf = ConfigManager("config/"+params[0]+".yaml")
+                    connection =  conf.show(params[1])["connection"]
+                    command = connection["adress"] + "_" + connection["user"]+ "_" + \
+                            connection["passwd"] + "_" + str(connection["sshport"])  + "_" + str(connection["remoteport"])
+                    try:
+                        conn.send(command)
+                        t = None
+                        while t == None:
+                            t = conn.get_state()
+                        print(t)
+                    except AttributeError as e:
+                       print("CONNECTING USING LOCAL MANAGER!")
+                except ConfigManagerError as e:
+                    print (e)
+                
+                except Exception as e:
+                    print (e)
+            else:
+                print ("error")
 
     def do_localConnect(self, arg):
         """Connecting via ssh"""
@@ -87,8 +98,12 @@ class Shell(cmd.Cmd):
     def emptyline(self):
         return False
 
-    def connectToList(self, listFile):
-        conf = ConfigManager(listFile)
+    def connectList(self, listFile):
+        try:
+            conf = ConfigManager(listFile)
+        except Exception as e:
+            print ("No conf file", listFile)
+            return 1
         
         server_list = []
         for server in conf.loader:
@@ -101,15 +116,17 @@ class Shell(cmd.Cmd):
             # adres_user_password_sshport_remoteport
             command = connection["adress"] + "_" + connection["user"]+ "_" + \
                     connection["passwd"] + "_" + str(connection["sshport"])  + "_" + str(connection["remoteport"])
-
-            conn.send(command)
-            t = None
-            while t == None:
-              t = conn.get_state()
-            #status_adres_localport
-            server_status = t.split("_")
-            connection_list[server_status[1]]=server_status[2]
-            print("Connecting to" , connection["adress"], "[", server_status[0], "]")
+            try:
+                conn.send(command)
+                t = None
+                while t == None:
+                  t = conn.get_state()
+                #status_adres_localport
+                server_status = t.split("_")
+                connection_list[server_status[1]]=server_status[2]
+                print("Connecting to" , connection["adress"], "[", server_status[0], "]")
+            except AttributeError as e:
+                print("CONNECTING USING LOCAL MANAGER!")
         print(connection_list)
 
 # if __name__ == '__main__':
