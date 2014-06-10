bf8104afcaa7920ff78b093e8fabacb736e6a85e
bleakdev <sebastian.zientek1@gmail.com>
2014-06-10 23:59:50 +0200

    Dumpy - prawie gotowe

--- configmanager.py
+++ configmanager.py
@@ -2,23 +2,24 @@ import yaml
 #najpierw & później * !!
 
 class ConfigManager:
-    def __init__(self, path):
-        try:
-            self.yamlfile = open(path,'r')
-        except FileNotFoundError:
-            raise ConfigManagerError("No file under this path")
-        self.loader = yaml.load(self.yamlfile)
-        self.yamlfile.close()
-    def get(self, what):
-        try:
-            return self.loader[what]
-        except KeyError:
-            err = what + " is not present in yaml file"
-            raise ConfigManagerError(err)
+	def __init__(self, path):
+		self.path = path
+		try:
+			self.yamlfile = open(path,'r')
+		except FileNotFoundError:
+			raise ConfigManagerError(path+' file does not exist')
+		self.loader = yaml.load(self.yamlfile)
+		self.yamlfile.close()
+	def get(self, what):
+		try:
+			return self.loader[what]
+		except KeyError:
+			err = what + ' is not exist in: '+self.path
+			raise ConfigManagerError(err)
 
-    def get_all(self):
-        return self.loader
+	def get_all(self):
+		return self.loader
 
 class ConfigManagerError(Exception):
-    def __init__(self, value):
-        self.value = value
+	def __init__(self, value):
+		self.value = value
--- mod_postgres.py
+++ mod_postgres.py
@@ -6,7 +6,7 @@ import psycopg2
 import os
 import paramiko
 import datetime
-import subprocess
+from subprocess import Popen, PIPE, call
 
 class Postgres(ModuleCore):
 	def __init__(self, completekey='tab', stdin=None, stdout=None):
@@ -19,8 +19,7 @@ class Postgres(ModuleCore):
 		database = cnf["databases"][base_name]
 
 		if conn["type"] == "ssh":
-			cmd = conn["adress"] + "_" + conn["user"] + "_" + conn["passwd"] + "_" + str(conn["sshport"]) + "_" + str(
-				conn["remoteport"]) + "_no"
+			cmd = conn["adress"] + "_" + conn["user"] + "_" + conn["passwd"] + "_" + str(conn["sshport"]) + "_" + str(conn["remoteport"]) + "_no"
 			common.conn.send(cmd)
 			ans = None
 			while ans == None:
@@ -48,110 +47,153 @@ class Postgres(ModuleCore):
 					print('Error: ', e)
 				except psycopg2.DatabaseError as e:
 					print('Error: ', e)
-			else:
+			else:	#próba bezpośredniego połączonia
 				pass
 
 			pass
 		else:
 			pass
 
+	def local_dump(self, db_name, db_user, db_pass, host, port, file_name):
+		os.putenv('PGPASSWORD', db_pass)
+		dumper = """ "./bin/pg_dump.exe" -U %s -d %s -h %s -p %s -f %s -C --column-inserts"""
+		command = dumper % (db_user, db_name, host, port, file_name)
+		call(command, shell=True)
+
+		# program = "./bin/pg_dump.exe"
+		# arg = "-U %s -d %s -h %s -p %s -f %s -C --column-inserts"
+		# p = Popen([program, arg % (db_user, db_name, host, port, file_name)], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+		# output, err = p.communicate(str.encode(db_pass))
+		# print(output, err)
+
+
+
 	def dump(self, file_name, serv_name, base_name, dump_file):
-		date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-		cnf = ConfigManager("config/" + file_name + ".yaml").get(serv_name)
-		conn = cnf["connection"]
-		database = cnf["databases"][base_name]
+		try:
+			date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+			cnf = ConfigManager("config/" + file_name + ".yaml").get(serv_name)
+			conn = cnf["connection"]
+			database = cnf["databases"][base_name]
+			dump_file_name = 'dump/'+dump_file+'_'+file_name+'_'+serv_name+'_'+base_name+'_'+date+'.sql'
 
-		os.putenv('PGPASSWORD', database["passwd"])
+			os.putenv('PGPASSWORD', database["passwd"])
 
-		if conn["type"] == "ssh": #Dla połączeń ssh
-			dumper = "pg_dump -U %s -d %s -C --column-inserts"
-			command = dumper % (database["user"], database["name"])
-			client = paramiko.SSHClient()
-			client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-			client.connect(conn["adress"], username=conn["user"] ,password=conn["passwd"], port=22)
-			channel = client.get_transport().open_session()
+			if conn["type"] == "ssh": #Dla połączeń ssh
+				dumper = "pg_dump -U %s -d %s -C --column-inserts"
+				command = dumper % (database["user"], database["name"])
+				client = paramiko.SSHClient()
+				client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+				client.connect(conn["adress"], username=conn["user"] ,password=conn["passwd"], port=22)
+				channel = client.get_transport().open_session()
 
 
-			channel.exec_command(command)
+				channel.exec_command(command)
 
-			stderr = b''
-			cmd = channel.recv_stderr(256)
-			while cmd != b'':
-				stderr += cmd
+				stderr = b''
 				cmd = channel.recv_stderr(256)
+				while cmd != b'':
+					stderr += cmd
+					cmd = channel.recv_stderr(256)
 
-			stdout = b''
-			cmd = channel.recv(256)
-			while cmd != b'':
-				stdout += cmd
-				cmd = channel.recv(256)
-			file = open(dump_file+'_'+file_name+'_'+serv_name+'_'+base_name+'_'+date+'.sql', 'w')
-			file.write(stdout.decode())
-			file.close()
+				if stderr == b'':
+					stdout = b''
+					cmd = channel.recv(256)
+					while cmd != b'':
+						stdout += cmd
+						cmd = channel.recv(256)
+					file = open(dump_file_name, 'w')
+					file.write(stdout.decode())
+					file.close()
+				else:
+					print("\t\tWARNING: pg_dump not exist on server\n\t\t\t attempt to use the local pg_dump")
 
-		elif conn["type"] == "direct": #Jeżeli nie ma ssh
-			dumper = """ "./bin/pg_dump.exe" -U %s -d %s -h %s -p %s -f %s -C --column-inserts"""
-			dump_file_name = dump_file+'_'+file_name+'_'+serv_name+'_'+base_name+'_'+date+'.sql'
-			command = dumper % (database["user"], database["name"], conn["adress"], conn["remoteport"], dump_file_name)
-			subprocess.call(command,shell = True)
+					cmd = conn["adress"] + "_" + conn["user"] + "_" + conn["passwd"] + "_" + str(conn["sshport"]) + "_" + str(conn["remoteport"]) + "_no"
+					common.conn.send(cmd)
+					ans = None
+					while ans == None:
+						ans = common.conn.get_state()
+
+					if ans.split('_')[0] == 'ok':
+						self.local_dump(database["name"], database["user"], database["passwd"], '127.0.0.1', int(ans.split("_")[2]), dump_file_name)
+						print("\t\tSUCCESS\n")
+					else:
+						print("\t\tFAIL\n")
+
+
+			elif conn["type"] == "direct": #Jeżeli nie ma ssh
+				self.local_dump(database["name"], database["user"], database["passwd"], conn["adress"], conn["remoteport"], dump_file_name)
+
+
+
+
+		except ConnectionRefusedError:
+			print("\t\tERROR: Connection Refused by host\n")
+
+		except Exception as e:
+			print(type(e))
+			print(e)
 
 
 
 	def do_dump(self, args):
 		"""dump <base> <file_name>"""
-		(values, values_num) = self.parse_args(args, 1, 2)
-		if len(values) == 2: #Jeżeli 2 argumenty (na wybranym konfigu)
-			conf_args = values[0].split('.')
+		try:
+			(values, values_num) = self.parse_args(args, 1, 2)
+			if len(values) == 2: #Jeżeli 2 argumenty (na wybranym konfigu)
+				conf_args = values[0].split('.')
 
-			if len(conf_args)== 3:
-				self.dump(conf_args[0], conf_args[1], conf_args[2], values[1])
-			elif len(conf_args) == 2:
-				conf = ConfigManager("config/" + conf_args[0] + ".yaml").get(conf_args[1])
-				databases = conf["databases"]  #konfiguracje baz danych
-				print(">Dumping databases:")
-				for db in databases:
-					print("\t" + db)
-					self.dump(conf_args[0], conf_args[1], db, values[1])
-			elif len(conf_args) == 1:
-				servers = ConfigManager("config/" + conf_args[0] + ".yaml").get_all()
-				print(">Dumping databases:")
-				for srv in servers:
-					print("\t" + srv)
-					databases = servers[srv]["databases"]
+				if len(conf_args)== 3:
+					self.dump(conf_args[0], conf_args[1], conf_args[2], values[1])
+				elif len(conf_args) == 2:
+					conf = ConfigManager("config/" + conf_args[0] + ".yaml").get(conf_args[1])
+					databases = conf["databases"]  #konfiguracje baz danych
+					print(">Dumping databases:")
 					for db in databases:
-						print("\t\t" + db)
-						self.dump(conf_args[0], srv, db, values[1])
-		elif len(values) == 1: #jeden argument (na wszystkich konfigach)
-			files = []
-			for file in os.listdir("./config"):
-				if file.endswith(".yaml"):
-					files.append(file.split(".")[0])
-
-			print("Dump:")
-			for file in files:
-				print(file)
-
-			ans = input("Are you sure? [NO/yes/info]: ")
-			if ans == "yes":
-				for file in files:
-					servers = ConfigManager("config/" + file + ".yaml").get_all()
+						print("\t" + db)
+						self.dump(conf_args[0], conf_args[1], db, values[1])
+				elif len(conf_args) == 1:
+					servers = ConfigManager("config/" + conf_args[0] + ".yaml").get_all()
+					print(">Dumping databases:")
 					for srv in servers:
 						print("\t" + srv)
 						databases = servers[srv]["databases"]
 						for db in databases:
 							print("\t\t" + db)
-							self.dump(file, srv, db, values[0])
-			elif ans == "info":
-				for file in files:
-					servers = ConfigManager("config/" + file + ".yaml").get_all()
-					for srv in servers:
-						print("\t" + srv)
-						databases = servers[srv]["databases"]
-						for db in databases:
-							print("\t\t" + db)
-			else:
-				print("aborted")
+							self.dump(conf_args[0], srv, db, values[1])
+			elif len(values) == 1: #jeden argument (na wszystkich konfigach)
+				files = []
+				for file in os.listdir("./config"):
+					if file.endswith(".yaml"):
+						files.append(file.split(".")[0])
 
+				print("Dump:")
+				for file in files:
+					print(file)
+
+				ans = input("Are you sure? [NO/yes/info]: ")
+				if ans == "yes":
+					for file in files:
+						servers = ConfigManager("config/" + file + ".yaml").get_all()
+						for srv in servers:
+							print("\t" + srv)
+							databases = servers[srv]["databases"]
+							for db in databases:
+								print("\t\t" + db)
+								self.dump(file, srv, db, values[0])
+				elif ans == "info":
+					for file in files:
+						servers = ConfigManager("config/" + file + ".yaml").get_all()
+						for srv in servers:
+							print("\t" + srv)
+							databases = servers[srv]["databases"]
+							for db in databases:
+								print("\t\t" + db)
+				else:
+					print("aborted")
+		except ParseArgsException as e:
+			print("Incorrect number of arguments.")
+		except Exception as e:
+			print(e)
 
 		# date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 		# self.dump('192.168.0.100','shepherd', 'test', 'test', 'shepherd', 'dbshepherd', values[1]+'_'+date+'.sql')
@@ -266,3 +308,7 @@ class Postgres(ModuleCore):
 			print(e)
 		except Exception as e:
 			print(e)
+
+class PostgressError(Exception):
+	def __init__(self, value):
+		self.value = value
