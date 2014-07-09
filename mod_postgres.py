d6a8270b1c44ac53556a0a0d3b04baae3c205700
bleakdev <sebastian.zientek1@gmail.com>
2014-07-09 22:13:13 +0200

    poprawki dump/restore

--- mod_postgres.py
+++ mod_postgres.py
@@ -7,16 +7,69 @@ import os
 import paramiko
 import datetime
 from subprocess import Popen, PIPE
-
-import sys
-import io
-import select
+import re
 
 class Postgres(ModuleCore):
 	def __init__(self, completekey='tab', stdin=None, stdout=None):
 		super().__init__()
 		self.set_name('Postgres')
 		self.warn = False
+
+	def get_local_version(self, cmd):
+		proc = Popen(cmd , stdout=PIPE, stderr=PIPE, shell=True)
+		out, err = proc.communicate()
+		if err != b'':
+			print('ERROR:', err.decode('utf8', 'ignore'))
+		else:
+			return re.findall(r'\d+.\d+.\d+', out.decode('utf8', 'ignore'))[0].split('.')
+
+	def get_pg_version(self, db_name, db_user, db_passwd, db_host, db_port):
+		try:
+			pg_conn = psycopg2.connect(dbname=db_name, user=db_user, host=db_host, password=db_passwd, port=db_port)
+			cur = pg_conn.cursor()
+			cur.execute('select version()')
+			return re.findall(r'\d+.\d+.\d+', cur.fetchone()[0])[0].split('.')
+		except psycopg2.Error as e:
+			print('--------------------')
+			print('Error:', e, end='')
+			print('--------------------')
+		except psycopg2.Warning as w:
+			print('--------------------')
+			print('Warning:', w, end='')
+			print('--------------------')
+		except psycopg2.InterfaceError as e:
+			print('--------------------')
+			print('Error:', e, end='')
+			print('--------------------')
+		except psycopg2.DatabaseError as e:
+			print('--------------------')
+			print('Error:', e, end='')
+			print('--------------------')
+
+	def is_valid_versions(self, ver1, ver2):
+		if ver1[0] > ver2[0]:
+			return True
+		elif ver1[0] == ver2[0]:
+			if ver1[1] >= ver2[1]:
+				return True
+			else:
+				return False
+		else:
+			return False
+
+
+	def do_pg_versions(self, arg):
+		out = self.get_local_version('psql --version')
+		if out != None:
+			print('Local psql version:', out[0]+'.'+out[1]+'.'+out[2])
+
+		out = self.get_local_version('pg_dump --version')
+		if out != None:
+			print('Local pg_dump version:', out[0]+'.'+out[1]+'.'+out[2])
+
+		out = self.get_local_version('pg_restore --version')
+		if out != None:
+			print('Local pg_restore version:', out[0]+'.'+out[1]+'.'+out[2])
 
 	def psycop_query(self, db_name, db_user, db_passwd, db_host, db_port, db_query):
 		try:
@@ -93,6 +146,8 @@ class Postgres(ModuleCore):
 
 	# ------------------------------------------------------------------------------------------------------------------
 
+
+
 	def local_dump(self, db_name, db_user, db_pass, host, port, file_name, type = 'sql'):
 		dumper = """pg_dump.exe -U %s -d %s -h %s -p %s -f %s -C --column-inserts"""
 
@@ -117,73 +172,58 @@ class Postgres(ModuleCore):
 		if stderr != b'':
 			raise PostgressError(stderr.decode('iso_8859_2', 'ignore'))
 
-	def remote_dump(self, db_name, db_user, db_pass, con_user, con_pass, host, sshport, remoteport, file_name, type = 'sql'):
-		dumper = "pg_dump -U %s -d %s -C --column-inserts"
-
-		if type == 'tar':
-			dumper += ' -Ft'
-			dump_file_name = file_name + '.tar'
-		else:
-			dump_file_name = file_name + '.sql'
-
-		os.putenv('PGPASSWORD', db_pass)
-		command = dumper % (db_user, db_name)
-		client = paramiko.SSHClient()
-		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		client.connect(host, username=con_user ,password=con_pass, port=sshport)
-		channel = client.get_transport().open_session()
-		channel.exec_command(command)
-
-		stderr = b''
-		cmd = channel.recv_stderr(256)
-		while cmd != b'':
-			stderr += cmd
-			cmd = channel.recv_stderr(256)
-
-		if stderr == b'':
-			stdout = b''
-			cmd = channel.recv(256)
-			while cmd != b'':
-				stdout += cmd
-				cmd = channel.recv(256)
-
-			file = open(dump_file_name, 'wb')
-			file.write(stdout)
-			file.close()
-		else: # nie udało się pg_dumpem z serwera, próba pg_dumpem lokalnym
-			if self.warn == True:
-				print('--------------------')
-				print('WARNING: '+stderr.decode('iso_8859_2', 'ignore')+'attempt to use the local pg_dump')
-			cmd = host + "_" + con_user + "_" + con_pass + "_" + str(sshport) + "_" + str(remoteport) + "_no"
-			common.conn.send(cmd)
-			ans = None
-			while ans == None:
-				ans = common.conn.get_state()
-
-			if ans.split('_')[0] == 'ok':
-				# self.local_dump(db_name, db_user, db_pass, '127.0.0.1', int(ans.split("_")[2]), dump_file_name, type)
-				self.local_dump(db_name, db_user, db_pass, '127.0.0.1', int(ans.split("_")[2]), file_name, type)
-				if self.warn == True:
-					print('SUCCESS')
-					print('--------------------')
-			else:
-				if self.warn != True:
-					print('--------------------')
-				print('FAIL')
-				print('--------------------')
-
 	def exe_dump(self, file_name, serv_name, base_name, backup_name, type):
+		date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+		dump_file_name = 'dump/'+backup_name+'_'+file_name+'_'+serv_name+'_'+base_name+'_'+date
+		dumper = """pg_dump.exe -U %s -d %s -h %s -p %s -f %s -C --column-inserts"""
+
 		try:
-			date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 			cnf = ConfigManager("config/" + file_name + ".yaml").get(serv_name)
 			conn = cnf["connection"]
 			db = cnf["databases"][base_name]
-			dump_file_name = 'dump/'+backup_name+'_'+file_name+'_'+serv_name+'_'+base_name+'_'+date
+
+			db_name = db["name"]
+			db_user = db["user"]
+			db_pass = db["passwd"]
+			conn_adr = ''
+			conn_port = None
+
 
 			if conn["type"] == "ssh": #Dla połączeń ssh
-				self.remote_dump(db["name"], db["user"], db["passwd"], conn["user"], conn["passwd"], conn["adress"], conn["sshport"], conn["remoteport"], dump_file_name, type)
+				cmd = conn["adress"] + "_" + conn["user"] + "_" + conn["passwd"] + "_" + str(conn["sshport"]) + "_" + str(conn["remoteport"]) + "_no"
+				common.conn.send(cmd)
+				ans = None
+				while ans == None:
+					ans = common.conn.get_state()
+
+				status, hostname, db_port = ans.split("_")
+
+				if status == "ok":  #udało się utworzyć tunel
+					conn_adr = 'localhost'
+					conn_port = db_port
+				else:
+					raise PostgressError('Unable to create ssh tunnel')
+
 			elif conn["type"] == "direct":
-				self.local_dump(db["name"], db["user"], db["passwd"], conn["adress"], conn["remoteport"], dump_file_name, type)
+				conn_adr = conn["adress"]
+				conn_port = conn["remoteport"]
+			# ------------
+			#db_name, db_user, db_host, db_passwd, db_port
+			local_version = self.get_local_version('pg_dump --version')
+			remote_version = self.get_pg_version(db_name, db_user, db_pass, conn_adr, conn_port)
+			print(remote_version)
+			print(self.is_valid_versions(local_version, remote_version))
+			# ------------
+
+			if type == 'tar':
+				dumper += ' -Ft'
+				dump_file_name += '.tar'
+			else:
+				dump_file_name += '.sql'
+
+			print(dumper)
+
+
 		except ConnectionRefusedError:
 			print('--------------------')
 			print('ERROR: Connection Refused by host')
@@ -298,45 +338,13 @@ class Postgres(ModuleCore):
 			if stderr != b'':
 				raise PostgressError(stderr.decode('utf8', 'ignore'))
 
-	def remote_restore(self, db_name, db_user, db_pass, con_user, con_pass, host, sshport, remoteport, file_name, type = 'sql'):
-		restorer = "pg_restore -U %s -d %s -h %s -p %s"
 
-
-		# os.putenv('PGPASSWORD', db_pass)
-		client = paramiko.SSHClient()
-		client.load_system_host_keys()
-		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		client.connect(host, username=con_user ,password=con_pass, port=sshport)
-		channel = client.get_transport().open_session()
-		channel.exec_command("pg_dump -W")
-		channel.send('pass\n'.encode())
-		channel.send('pass\n'.encode())
-
-
-		stderr = b''
-		cmd = channel.recv_stderr(1)
-		while cmd != b'':
-			stderr += cmd
-			cmd = channel.recv_stderr(1)
-
-		if stderr == b'':
-			stdout = b''
-			cmd = channel.recv(1)
-			while cmd != b'':
-				stdout += cmd
-				cmd = channel.recv(1)
-
-		print(stderr.decode('utf-8', 'ignore'), stdout.decode('utf-8', 'ignore'))
-
-
-
-
-	def do_restore(self, args):
-		try:
-			# self.local_restore('postgres', 'postgres', 'root', '127.0.0.1', 5432, 'local.sql', type = 'sql')
-			self.remote_restore('postgres', 'postgres', 'root', 'seba', 'seba', '127.0.0.1', 23, 5432, 'proba.tar', 'tar')
-		except PostgressError as e:
-			print(e)
+	# def do_restore(self, args):
+	# 	try:
+	# 		# self.local_restore('postgres', 'postgres', 'root', '127.0.0.1', 5432, 'local.sql', type = 'sql')
+	# 		# self.remote_restore('postgres', 'postgres', 'root', 'seba', 'seba', '127.0.0.1', 23, 5432, 'proba.tar', 'tar')
+	# 	except PostgressError as e:
+	# 		print(e)
 
 
 class PostgressError(Exception):
