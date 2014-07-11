d66a8b1a1c7de79f700767babb0eaec1dc4fdf41
kaxiorek <kaxiorek@gmail.com>
2014-07-11 10:12:03 +0200

    hotfix

--- test_postgres.py
+++ test_postgres.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 from mod_postgres import Postgres
-
+import common
 test1_dump_file = "test1_dump"
 
 def setUpModule():
@@ -12,20 +12,22 @@ class PostgresTest(unittest.TestCase):
 
 	def test1_dump(self):
 		#if exists remove test1_dump* file
-		for file in os.listdir("dump"):
+		for file in os.listdir("."):
 					if file.startswith("test1_dump"):
-						if os.path.exists("dump/" + file.title()):
-							os.remove("dump/" + file.title())
+						if os.path.exists("./" + file.title()):
+							os.remove("./" + file.title())
 		
 		pg = Postgres()
+		pg.master = "test"
 		test = pg.do_dump("lista_test.ShouldBeOK.pgBase_test " + test1_dump_file)
+		
 		#test if file exists
 		counter = 0
-		for file in os.listdir("dump"):
+		for file in os.listdir("."):
 					if file.startswith("test1_dump"):
 						counter += 1
-						if os.path.exists("dump/" + file.title()):
-							os.remove("dump/" + file.title())
+						if os.path.exists("./" + file.title()):
+							os.remove("./" + file.title())
 		self.assertNotEqual(counter,0)
 	
 	def test2_query(self):
